124e3e66ae34f5d31ad3276be7f484447a2a753f
Nightmare <90391492+SINON3@users.noreply.github.com>
2023-08-31 18:52:30 +0530

    Yelan 3

--- info.py
+++ info.py
@@ -78,11 +78,11 @@ if SELF_DELETE == "True":
     SELF_DELETE = True
 
     # Download Tutorial Button #
-DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://omegalinks.in/ref/GreyMatter6458"
+DOWNLOAD_TEXT_NAME = "✨ Download ✨"
+DOWNLOAD_TEXT_URL = "https://telegram.me/HaremHanimes"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON = "Join Our Channel"
+CAPTION_BUTTON_URL = "https://telegram.me/HaremHanimes"
 
    # Auto Delete For Bot Sending Files #
