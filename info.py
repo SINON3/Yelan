b82841319b04df7bc4e5225211604dc65349b532
Nightmare <90391492+SINON3@users.noreply.github.com>
2023-09-01 11:33:55 +0530

    Yelan

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "asadashino")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001936523805'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1001904285591'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'search_zone_support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
